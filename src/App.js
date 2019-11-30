990d7be420ed36d564137f9328b87647b38e0a07
lenhuy90 <34921431+lenhuy90@users.noreply.github.com>
2019-11-30 17:03:41 +0700

    la3

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ProductImage,ProductInfo} from "./Item"
+import data from "./data.json";
+
+/*
+
+const b = [1,2,3,4].map(elm => elm + 1)
+
+b  = [2,3,4,5]
+*/
 
 function App() {
+  const productList = data.data;
   return (
     <div className="App">
-      <ProductImage src="https://noithatdailoi.com/wp-content/uploads/ghe-eames-3-5.jpg" saleText="SALE"></ProductImage>
-      <ProductInfo type="FUNITURE" name="Mini Ghế Làm Việc" finalPrice="112.000 USD" price="200.000usd"></ProductInfo>
+      {productList.map(elm => (
+        <div className="product-item">
+          <p>ID: {elm.id}</p>
+          <ProductImage src={elm.img_url} saleText={elm.saleText} />
+          <ProductInfo
+            type={elm.type}
+            name={elm.name}
+            finalPrice={elm.finalPrice}
+            price={elm.price}
+          />
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default App
